300aaf83c1ed22cead29f1e798af60d13b0907f5
Alexandre Gigliotti <alex@panosoft.com>
2016-02-01 13:54:09 -0800

    Add tests.

--- test/index.js
+++ test/index.js
@@ -1,17 +1,20 @@
 const co = require('co');
-const fs = require('fs');
-const open = require('open');
-const path = require('path');
+const expect = require('chai').expect;
+const pdfText = require('pdf-text');
 const render = require('../lib');
 
-const input = require('./input.json');
+const pdfToText = pdf => new Promise((resolve, reject) =>
+  pdfText(pdf, (error, chunks) => error ? reject(error) : resolve(chunks))
+);
 
-co(function * () {
-  try {
-    const output = yield render(input);
-    const testFilename = path.resolve(__dirname, './test.pdf');
-    fs.writeFileSync(testFilename, output);
-    open(testFilename);
-  }
-  catch (error) { console.error(error); }
+describe('pdf-form-fill', () => {
+  describe('render', () => {
+    it('render form', co.wrap(function * () {
+      this.timeout(10000);
+      const input = require('./input.json');
+      const pdf = yield render(input);
+      const text = yield pdfToText(pdf);
+      expect(text).to.contain(input.formData.fullName);
+    }));
+  });
 });
